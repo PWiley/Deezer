efccf1489541463cda8572e7a903d6a6f9a92884
bbloch <b9bloch@yahoo.fr>
2021-12-02 09:21:17 +0100

    Create Project

---
+++ Deezer/Deezer/Sources/AppDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.swift
+//  Deezer
+//
+//  Created by Bertrand BLOC'H on 02/12/2021.
+//
+
+import UIKit
+
+@main
+final class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+    ) -> Bool {
+        return true
+    }
+}
+
