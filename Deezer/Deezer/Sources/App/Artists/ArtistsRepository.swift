c5dc8928148f125a3a608abed89b414ff881376f
bbloch <b9bloch@yahoo.fr>
2021-12-02 09:59:51 +0100

    Start Artists

--- Deezer/Deezer/Sources/App/Artists/ArtistsRepository.swift
+++ Deezer/Deezer/Sources/App/Artists/ArtistsRepository.swift
@@ -6,3 +6,17 @@
 //
 
 import Foundation
+
+typealias VoidClosure = () -> Void
+typealias InputClosure<T> = (T) -> Void
+
+enum ArtistsRepositoryError: Error {
+    
+}
+
+protocol ArtistsRepositoryType {
+    func searchArtists(
+        for name: String,
+        callback: InputClosure<Result<ArtistsResponse, ArtistsRepositoryError>>
+    )
+}
